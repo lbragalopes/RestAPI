643e0149bdedc11b0c249452c5e5327bcb0ea140
Luciana Lopes <lbragalopes@gmail.com>
2023-04-17 21:36:11 -0300

    refatoração

--- RestFull_API/Project/vintage_cars_database.py
+++ RestFull_API/Project/vintage_cars_database.py
@@ -3,7 +3,7 @@ import json
 import re
 
 key_nomes = ['id', 'placa', 'marca', 'modelo', 'ano', 'automatico']
-key_widths = [10, 10, 15, 15, 10, 20]
+key_widths = [10, 10, 15, 15, 20, 20]
 h_close = {'Connection': 'Close'}
 h_content = {'Content-Type': 'application/json'}
 
@@ -32,7 +32,7 @@ def print_menu():
 def read_user_opcao():
     opcao = input("Digite a opção: ")
     while opcao not in ['0', '1', '2', '3', '4', '5', '6']:
-        opcao = input("Opção inválida! Tente novamente, conforme tabela acima: ")
+        opcao = input("Digite a opção (0-6): ")
     return opcao
 
 def print_header():
@@ -85,7 +85,6 @@ def enter_id():
         print("Erro ao obter registros: ", response.status_code)
         return None
 
-
 def enter_placa():
     registro_placa = input("Placa do veículo (XXX-0000): ").upper()
     while not re.match(r"^[A-Z]{3}-\d{4}$", registro_placa):
@@ -102,7 +101,7 @@ def placa_exists(placa):
         return False
 
 def enter_ano():
-    year = input("Ano de producao do registro: ")
+    year = input("Ano de producao do veiculo: ")
     if not year:
         return None
     while not (year.isdigit() and 1900 <= int(year) <= 2023):
@@ -120,57 +119,67 @@ def enter_nome(what):
     return nome
 
 def enter_automatico():
-    automatico = input("O registro é automatico (S/N): ")
+    automatico = input("O veículo é automatico (S/N): ")
     if not automatico:
         return None
     while automatico.lower() not in ['s', 'n']:
         print("Resposta inválida. Apenas 'S' or 'N'.")
-        automatico = input("Is the registro automatico (S/N): ")
+        automatico = input("O veiculo é automatico (S/N): ")
     return 'True' if automatico.lower() == 's' else 'False'
 
 def add_registro():
-    registro = {}
-    registro['id'] = enter_id()
-    registro['placa'] = enter_placa()
-    if placa_exists(registro['placa']):
-        print("Erro: placa já em uso")
-        return      
-    registro['marca'] = enter_nome('marca')
-    registro['modelo'] = enter_nome('modelo')
-    registro['ano'] = enter_ano()
-    registro['automatico'] = enter_automatico()
-    if not all(registro.values()):
-        print("Falha ao adicionar o registro")
-        return
-    response = requests.post('http://localhost:3000/registros', headers=h_content, data=json.dumps(registro))
-    if response.status_code == 201:
-        print("Registro adicionado com sucesso")
-    else:
-        print("Falha ao adicionar o registro")
+    while True:
+        registro = {}
+        registro['id'] = enter_id()
+        registro['placa'] = enter_placa()
+        if placa_exists(registro['placa']):
+            print(f"Erro: Já existe um cadastro com a placa {registro['placa']}")
+            return      
+        registro['marca'] = enter_nome('marca')
+        registro['modelo'] = enter_nome('modelo')
+        registro['ano'] = enter_ano()
+        registro['automatico'] = enter_automatico()
+        if not all(registro.values()):
+            print("Falha ao cadastrar o veículo")
+            return
+        response = requests.post('http://localhost:3000/registros', headers=h_content, data=json.dumps(registro))
+        if response.status_code == 201:
+            print("Registro adicionado com sucesso")
+        else:
+            print("Falha ao adicionar o registro")
+        
+        opcao = input("Gostaria de adicionar outro registro? (s/n) ")
+        if opcao.lower() != 's':
+            break
 
 def delete_registro():
-    placa = enter_placa()
-    response = requests.get(f'http://localhost:3000/registros?placa={placa}', headers=h_content)
-    if response.status_code == 200 and response.json():
-        registrados = response.json()
-        print_header()
-        for registro in registrados:
-            print_registro(registro)
-        confirmacao = input("Para confirmar a exclusão do registro, digite excluir: ")
-        if confirmacao == 'excluir':
-            registro = response.json()[0]
-            response = requests.delete(f'http://localhost:3000/registros/{registro["id"]}', headers=h_content)
-            if response.status_code == 200:
-             print(f"Registro com placa {placa} excluído com sucesso")
-            else:
-                print("Falha ao excluir o registro")
-    else:
-        print(f"Não há registros com a placa {placa}")
+    while True:
+        placa = enter_placa()
+        response = requests.get(f'http://localhost:3000/registros?placa={placa}', headers=h_content)
+        if response.status_code == 200 and response.json():
+            registrados = response.json()
+            print_header()
+            for registro in registrados:
+                print_registro(registro)
+            confirmacao = input("Para confirmar a exclusão do registro, digite excluir: ")
+            if confirmacao == 'excluir':
+                registro = response.json()[0]
+                response = requests.delete(f'http://localhost:3000/registros/{registro["id"]}', headers=h_content)
+                if response.status_code == 200:
+                 print(f"Registro com placa {placa} excluído com sucesso")
+                else:
+                    print("Falha ao excluir o registro")
+        else:
+            print(f"Não há registros com a placa {placa}")
+        
+        opcao = input("Gostaria de excluir outro registro? (s/n) ")
+        if opcao.lower() != 's':
+            break
 
 def delete_all_registro():
     response = requests.get(f'http://localhost:3000/registros', headers=h_content)
     if response.status_code == 404:
-        print("Não existe registro do registro")
+        print("Não existem registros para excluir")
         return
    
     registros = response.json()
@@ -191,45 +200,57 @@ def delete_all_registro():
         print('Você não digitou a palavra excluir. Não foi possível excluir os registros.')
 
 def update_registro():
-    placa = enter_placa()
-    response = requests.get(f'http://localhost:3000/registros?placa={placa}', headers=h_content)
-    if response.status_code == 200 and response.json():
-        registrados = response.json()
-        print_header()
-        for registro in registrados:
-            print_registro(registro)
-            print("Atualize os campos ou deixe vazio para manter os valores antigos:")
+    while True:
+        placa = enter_placa()
+        response = requests.get(f'http://localhost:3000/registros?placa={placa}', headers=h_content)
+        if response.status_code == 200 and response.json():
+            registrados = response.json()
+            print_header()
+            for registro in registrados:
+                print_registro(registro)
+                print("Atualize os campos ou deixe vazio para manter os valores:")
+            
+            registro = response.json()[0]
+            new_registro = {}
+            new_registro['placa'] = registro['placa']
+            new_registro['marca'] = enter_nome('marca') or registro['marca']
+            new_registro['modelo'] = enter_nome('modelo') or registro['modelo']
+            new_registro['ano'] = enter_ano() or registro['ano']
+            new_registro['automatico'] = enter_automatico() or registro['automatico']
+            
+            if not all(new_registro.values()):
+                print("Falha ao atualizar o registro")
+                return
         
-        registro = response.json()[0]
-        new_registro = {}
-        new_registro['placa'] = registro['placa']
-        new_registro['marca'] = enter_nome('marca') or registro['marca']
-        new_registro['modelo'] = enter_nome('modelo') or registro['modelo']
-        new_registro['ano'] = enter_ano() or registro['ano']
-        new_registro['automatico'] = enter_automatico() or registro['automatico']
-        
-        if not all(new_registro.values()):
-            print("Falha ao atualizar o registro")
-            return
-    
-    response = requests.put(f'http://localhost:3000/registros/{registro["id"]}', headers=h_content, data=json.dumps(new_registro))
-    if response.status_code == 200:
-        print("registro atualizado com sucesso")
-    else:
-        print("Falha ao atualizar o registro")
+            response = requests.put(f'http://localhost:3000/registros/{registro["id"]}', headers=h_content, data=json.dumps(new_registro))
+            if response.status_code == 200:
+                print("Registro atualizado com sucesso")
+            else:
+                print("Falha ao atualizar o registro")
+        else:
+            print(f"Não foi encontrado veículo com a placa {placa}")
+      
+        opcao = input("Gostaria de alterar outro registro? (s/n) ")
+        if opcao.lower() != 's':
+            break
 
 def busca_placa():
-    placa = input("Digite a placa do veículo (XXX-0000): ").upper()
-    while not re.match(r"^[A-Z]{3}-\d{4}$", placa):
-        print("Placa inválida. Por favor, digite no formato XXX-0000.")
+    while True:
         placa = input("Digite a placa do veículo (XXX-0000): ").upper()
-    response = requests.get(f'http://localhost:3000/registros?placa={placa}', headers=h_content)
-    if response.status_code == 200 and response.json():
-        registro = response.json()[0]
-        print_header()
-        print_registro(registro)
-    else:
-        print(f"Não foi encontrado um veículo com a placa {placa}")
+        while not re.match(r"^[A-Z]{3}-\d{4}$", placa):
+            print("Placa inválida. Por favor, digite no formato XXX-0000.")
+            placa = input("Digite a placa do veículo (XXX-0000): ").upper()
+        response = requests.get(f'http://localhost:3000/registros?placa={placa}', headers=h_content)
+        if response.status_code == 200 and response.json():
+            registro = response.json()[0]
+            print_header()
+            print_registro(registro)
+        else:
+            print(f"Não foi encontrado veículo com a placa {placa}")
+        
+        opcao = input("Gostaria de buscar outro registro? (s/n) ")
+        if opcao.lower() != 's':
+            break
 
 
 while True:
@@ -243,6 +264,9 @@ while True:
         exit(0)
     elif opcao == '1':
         listar_registros()
+        opcao = input("Gostaria de retornar para a tela inicial? (s/n) ")
+        if opcao.lower() != 's':
+            break
     elif opcao == '2':
         busca_placa()
     elif opcao == '3':
